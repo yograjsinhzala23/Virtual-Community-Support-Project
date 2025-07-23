5ee38922b10ed30ec9c09030b77f070d6066cc51
Yograjsinh Zala <yograjsinhzala23@gmail.com>
2025-07-23 20:53:14 +0530

    second commit

---
+++ Mission/Mission.Entities/Entities/UserSkill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities.Entities
+{
+    public class UserSkill
+    {
+        public int UserId { get; set; }
+        public int SkillId { get; set; }
+
+        public User User { get; set; }
+        public MissionSkill Skill { get; set; }
+    }
+}
---
+++ Mission/Mission.Services/IServices/ICommonService.cs
@@ -0,0 +1,28 @@
+﻿using Mission.Entities.Models.CommonModels;
+using System.Collections.Generic;
+
+namespace Mission.Service.IServices
+{
+    public interface ICommonService
+    {
+        List<DropDownResponseModel> CountryList();
+
+        List<DropDownResponseModel> CityList(int countryId);
+
+        List<DropDownResponseModel> MissionCountryList();
+
+        List<DropDownResponseModel> MissionCityList();
+
+        List<DropDownResponseModel> MissionThemeList();
+
+        List<DropDownResponseModel> MissionSkillList();
+
+        List<DropDownResponseModel> MissionTitleList();
+
+       // List<DropDownResponseModel> GetUserSkill(int userId);
+
+
+
+
+    }
+}
